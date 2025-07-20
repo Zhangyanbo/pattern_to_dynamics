1051ada4df3007ca9f8fbafa70304f05aa245c88
Zhangyanbo <Zhang.Yanbo@asu.edu>
2025-07-20 00:57:50 +0000

    remove limitation on presets

--- 2D_continous/turing_pattern/generate_dataset.py
+++ 2D_continous/turing_pattern/generate_dataset.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     parser.add_argument("--num_samples", type=int, default=8192, help="Number of samples to generate")
     parser.add_argument("--height", type=int, default=128, help="Height of the grid")
     parser.add_argument("--width", type=int, default=128, help="Width of the grid")
-    parser.add_argument("--preset", type=str, default='waves', choices=['waves', 'life', 'maze'], help="Preset pattern type")
+    parser.add_argument("--preset", type=str, default='waves', help="Preset pattern type")
     parser.add_argument("--cuda", action='store_true', help="Use CUDA for simulation")
     parser.add_argument("--steps", type=int, default=2000, help="Number of simulation steps")
     parser.add_argument("--chunk", type=int, default=512, help="Chunk size for processing")
