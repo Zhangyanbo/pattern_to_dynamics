2cad62234d8d995397fadb730d5a4e1b458e0fc8
Zhangyanbo <Zhang.Yanbo@asu.edu>
2025-07-22 02:57:57 +0000

    add gradient_accumulation_steps; clean code; remove scheduler; scale the loss

--- 2D_continous/dynamics.py
+++ 2D_continous/dynamics.py
@@ -23,7 +23,13 @@ class Wrapper2D(nn.Module):
 class Trainer:
     def __init__(self, flow_model, score_model, shape:tuple, dataset, 
                 use_wandb:bool=False,
-                device='cuda', num_samples=2, lr=1e-3, weight_decay=1e-4, alpha=0.8, warmup_steps=500):
+                device='cuda', 
+                num_samples=2, 
+                lr=1e-3, 
+                weight_decay=1e-4, 
+                alpha=0.8, 
+                gradient_accumulation_steps=1,
+                warmup_steps=500):
         self._check_flow_model(flow_model)
         self.flow_model = Wrapper2D(flow_model, shape).to(device)
         self.score_model = Wrapper2D(score_model, shape).to(device)
@@ -34,6 +40,7 @@ class Trainer:
         self.alpha = alpha
 
         self.lr = lr
+        self.gradient_accumulation_steps = gradient_accumulation_steps
         self.weight_decay = weight_decay
         self.warmup_steps = warmup_steps
         self.use_wandb = use_wandb
@@ -49,6 +56,7 @@ class Trainer:
                 raise ValueError("ReLU activations don't support 2nd-order differentiation. Use SiLU or other differentiable activations instead.")
     
     def estimate(self, x):
+        xt = self.diffuser.add_noise(x, torch.randn_like(x))
         x = x.reshape(x.shape[0], -1)
         return div_estimate(self.flow_model, self.score_model, x, num_samples=self.num_samples, 
                             alpha=self.alpha)
@@ -64,33 +72,33 @@ class Trainer:
     
     def train(self, epochs=10, batch_size=32):
         self.setup_wandb()
-        optimizer = torch.optim.AdamW(self.flow_model.parameters(), lr=self.lr, weight_decay=self.weight_decay)
+        optimizer = torch.optim.AdamW(
+            self.flow_model.parameters(), 
+            lr=self.lr, 
+            weight_decay=self.weight_decay)
         dataloader = torch.utils.data.DataLoader(self.dataset, batch_size=batch_size, shuffle=True)
-        scheduler = get_cosine_schedule_with_warmup(
-            optimizer,
-            num_warmup_steps=self.warmup_steps,
-            num_training_steps=epochs * len(dataloader)
-        )
         self.flow_model.train()
 
         lf = nn.MSELoss()
+        step = 0
         
         for epoch in range(epochs):
             i = 0
             for batch in tqdm(dataloader, desc=f"Training Epoch {epoch+1}/{epochs}"):
-                scheduler.step()
+                step += 1
                 batch = batch.to(self.device)
-                batch = self.diffuser.add_noise(batch, torch.randn_like(batch))
-                optimizer.zero_grad()
                 
                 output = self.estimate(batch)
-                loss = lf(output['div(pv)'] / self.dim, torch.zeros_like(output['div(pv)']))
+                loss = lf(output['div(pv)'] / self.dim ** 0.5, torch.zeros_like(output['div(pv)']))
                 loss.backward()
 
                 if self.use_wandb:
                     wandb.log({'loss': loss.item(), 'epoch': epoch+i/len(dataloader), 'lr': optimizer.param_groups[0]['lr']})
                 
-                optimizer.step()
+                if (step + 1) % self.gradient_accumulation_steps == 0:
+                    optimizer.step()
+                    optimizer.zero_grad()
+                    step = 0
                 i += 1
             
             print(f'Epoch {epoch+1}/{epochs}, Loss: {loss.item()}')
