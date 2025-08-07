3e33377ee989117c3d3a0fd309a8fbc14793da55
Zhangyanbo <Zhang.Yanbo@asu.edu>
2025-08-07 00:55:35 +0000

    add options to use different x to x_t. add more outputs

--- 2D_continous/probflow/div.py
+++ 2D_continous/probflow/div.py
@@ -13,7 +13,7 @@ def hutchinson_estimate(model, x, num_samples: int = 64):
     return div_samples.mean(0), y_all[0]
 
 
-def div_estimate(flow_model, diff_model, x, num_samples=2, alpha=0.8):
+def div_estimate(flow_model, diff_model, x, num_samples=2, alpha=0.8, x0=None):
     """
     Calculate ∇·(v(x) * p(x)), where v(x) is the flow model and p(x) is
     represented implicitly by the score model.
@@ -26,11 +26,19 @@ def div_estimate(flow_model, diff_model, x, num_samples=2, alpha=0.8):
     num_batch = x.shape[0]
     with torch.no_grad():
         s = -diff_model(x) / ((1 - alpha) ** 0.5) # [NOTE] ∇log(p(x)) = -Ɛ(x) / (1 - alpha_t).sqrt() according to the Tweedie equation
-    div_term, v = hutchinson_estimate(flow_model, x, num_samples=num_samples)
+    
+    if x0 is None:
+        # If x0 is provided, we use it to compute the divergence
+        div_term, v = hutchinson_estimate(flow_model, x, num_samples=num_samples)
+    else:
+        # Otherwise, we use the diffused version of x, i.e., x_t
+        div_term, v = hutchinson_estimate(flow_model, x0, num_samples=num_samples)
     oth_term = torch.einsum('nd, nd -> n', v, s)
 
     output = {
         'div(pv)': div_term + oth_term,
+        'div(v)': div_term,
+        'v@s': oth_term,
         'score': s,
         'v': v,
     }
