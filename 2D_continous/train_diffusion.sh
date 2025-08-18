abab4dde23a9978fbfb1fced76b7c4eb3c28048a
Zhangyanbo <Zhang.Yanbo@asu.edu>
2025-08-18 19:28:11 +0000

    add batched training code

---
+++ 2D_continous/train_diffusion.sh
@@ -0,0 +1,4 @@
+python turing.py --sampling ddpm --objective diffusion --model waves --wb -n 100 --lr_schedule --lr 0.0002
+python turing.py --sampling ddpm --objective diffusion --model life --wb -n 100 --lr_schedule --lr 0.0002
+python turing.py --sampling ddpm --objective diffusion --model maze --wb -n 100 --lr_schedule --lr 0.0002
+python turing.py --sampling ddpm --objective diffusion --model spirals --wb -n 100 --lr_schedule --lr 0.0002
---
+++ 2D_continous/train_dynamics.sh
@@ -0,0 +1,3 @@
+python train_dynamics.py --dataset life -s --epochs 1000
+python train_dynamics.py --dataset waves -s --epochs 1000
+python train_dynamics.py --dataset spirals -s --epochs 1000
