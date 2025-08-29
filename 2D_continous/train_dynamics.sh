123b658ae40efa66b6bec876f2402cf66b4e7d8d
Zhangyanbo <Zhang.Yanbo@asu.edu>
2025-08-29 01:51:03 +0000

    update dynamical training epochs

--- 2D_continous/train_dynamics.sh
+++ 2D_continous/train_dynamics.sh
@@ -1,3 +1,3 @@
-python train_dynamics.py --dataset life -s --epochs 300
-python train_dynamics.py --dataset waves -s --epochs 300
-python train_dynamics.py --dataset spirals -s --epochs 300
+python train_dynamics.py --dataset life -s --epochs 1000
+python train_dynamics.py --dataset waves -s --epochs 1000
+python train_dynamics.py --dataset spirals -s --epochs 1000
