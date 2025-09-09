a6656bc57173655fc6532611e51da10bf39da8e8
Zhangyanbo <Zhang.Yanbo@asu.edu>
2025-09-09 15:26:49 +0000

    update training parameters

--- 2D_continous/train_dynamics.sh
+++ 2D_continous/train_dynamics.sh
@@ -1,3 +1,3 @@
-python train_dynamics.py --dataset life -s --epochs 1000
-python train_dynamics.py --dataset waves -s --epochs 1000
-python train_dynamics.py --dataset spirals -s --epochs 1000
+python train_dynamics.py --dataset life -s --epochs 1000 --bn --lr 0.01
+python train_dynamics.py --dataset waves --epochs 1000 --bn --lr 0.01
+python train_dynamics.py --dataset spirals --epochs 1000 --bn --lr 0.01
