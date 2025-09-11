86058f3f4555809fa82fa717aab11c8609a99ca2
Zhangyanbo <Zhang.Yanbo@asu.edu>
2025-09-11 00:06:40 +0000

    change name

--- 2D_continous/train_diffusion.sh
+++ 2D_continous/train_diffusion.sh
@@ -1,4 +1,4 @@
-python turing.py --sampling ddpm --model waves --wb -n 100 --lr_schedule --lr 0.001 --prediction_type v_prediction
-python turing.py --sampling ddpm --model life --wb -n 100 --lr_schedule --lr 0.001 --prediction_type v_prediction
-python turing.py --sampling ddpm --model maze --wb -n 100 --lr_schedule --lr 0.001 --prediction_type v_prediction
-python turing.py --sampling ddpm --model spirals --wb -n 100 --lr_schedule --lr 0.001 --prediction_type v_prediction
+python train_turing_diffusion.py --sampling ddpm --model waves --wb -n 100 --lr_schedule --lr 0.001 --prediction_type v_prediction
+python train_turing_diffusion.py --sampling ddpm --model life --wb -n 100 --lr_schedule --lr 0.001 --prediction_type v_prediction
+python train_turing_diffusion.py --sampling ddpm --model maze --wb -n 100 --lr_schedule --lr 0.001 --prediction_type v_prediction
+python train_turing_diffusion.py --sampling ddpm --model spirals --wb -n 100 --lr_schedule --lr 0.001 --prediction_type v_prediction
