a520bce618d1c71243bb51c641d9db6139145833
Zhangyanbo <Zhang.Yanbo@asu.edu>
2025-09-14 15:47:06 +0000

    add training config for alife diffusion models

---
+++ 2D_continous/train_alifes_diffusion.sh
@@ -0,0 +1,4 @@
+python train_diffusion.py --sampling ddpm --model dot --task alifes --wb -n 20 --lr_schedule --lr 0.001 --prediction_type v_prediction
+python train_diffusion.py --sampling ddpm --model dots --task alifes --wb -n 20 --lr_schedule --lr 0.001 --prediction_type v_prediction
+python train_diffusion.py --sampling ddpm --model taiji --task alifes --wb -n 20 --lr_schedule --lr 0.001 --prediction_type v_prediction
+python train_diffusion.py --sampling ddpm --model sym --task alifes --wb -n 20 --lr_schedule --lr 0.001 --prediction_type v_prediction
